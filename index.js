9f341d504876adf426ba494e1f562bc4167611f1
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-06-03 11:30:53 -0400

    randomize!

--- index.js
+++ index.js
@@ -20,32 +20,28 @@ function recurse(name) {
     axios.get(`${config.protocol}${config.domain}${config.path}${name}`).then(res => {
         if (res.status == 200) {
             let dom = new JSDOM(res.data);
-            let first = "";
+            let arr = [];
             [...dom.window.document.links].forEach(l => {
-                if (first == "") {
-                    let good = true;
-                    config.ignore.forEach(e => {
-                        if (l.href.includes(e)) {
-                            good = false;
-                        }
-                    })
-                    if (l.href.startsWith(`${config.path}`) && good) {
-                        let val = `${l.href.slice(6).charAt(0).toLowerCase()}${l.href.slice(6).slice(1)}`;
-                        if (visited.has(val)) {
-                            console.log("\nCycle reached!");
-                            process.exit(0);
-                        }
-                        else {
-                            first = val;
-                        }
+                let good = true;
+                config.ignore.forEach(e => {
+                    if (l.href.includes(e)) {
+                        good = false;
+                    }
+                })
+                if (l.href.startsWith(`${config.path}`) && good) {
+                    let val = `${l.href.slice(6).charAt(0).toLowerCase()}${l.href.slice(6).slice(1)}`;
+                    if (!visited.has(val)) {
+                        arr.push(val);
                     }
                 }
+
             })
-            if (first == "") {
+            if (arr.length == 0) {
                 console.log("\nDead end reached!");
                 process.exit(0);
             }
             else {
+                let first = arr[Math.floor(Math.random() * arr.length)];
                 console.log(first);
                 visited.add(first);
                 recurse(first);
